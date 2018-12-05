02b940ec9ead2c0a64878227880868db666103af
Muatasim Qazi <muatasimqazi@gmail.com>
2018-12-05 08:05:58 -0800

    working code

--- app/bin/src/main/java/edu/uw/oazeemi/trashcache/MainActivity.kt
+++ app/bin/src/main/java/edu/uw/oazeemi/trashcache/MainActivity.kt
@@ -1,25 +1,29 @@
 package edu.uw.oazeemi.trashcache
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 
 import kotlinx.android.synthetic.main.activity_main.*
+
+
 class MainActivity : AppCompatActivity() {
+    private val REQUEST_IMAGE_CAPTURE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        val intent = Intent(this, ChoiceActivity::class.java)
+        startActivity(intent)
     }
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
--- app/bin/src/main/java/edu/uw/oazeemi/trashcache/RegisterActivity.kt
+++ app/bin/src/main/java/edu/uw/oazeemi/trashcache/RegisterActivity.kt
@@ -7,16 +7,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
 
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
+    data class User(val uid: String, val username: String)
+    companion object {
+        val TAG = "RegisterActivity"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        setSupportActionBar(toolbar)
-
+//        setSupportActionBar(toolbar)
 
         register_button_register.setOnClickListener {
             performRegister()
@@ -37,22 +44,45 @@ class RegisterActivity : AppCompatActivity() {
 
         if(email.isEmpty() || password.isEmpty()) {
             Toast.makeText(this, "Please enter text in e-mail/password", Toast.LENGTH_SHORT).show()
+            return
         }
 
-        Log.d("RegisterActivity", "Email is: " + email)
-        Log.d("RegisterActivity", "Password: $password")
+        Log.d(TAG, "Attempting to create user with email: $email")
 
         // Firebase Authetication to create a uers with e-mail and password.
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) {
-                    if(!it.isSuccessful) return@addOnCompleteListener
+                    if(!it.isSuccessful) {
+                        return@addOnCompleteListener
+                    } else {
+                        var intent = Intent(this, ChoiceActivity::class.java)
+                        intent.putExtra("id", FirebaseAuth.getInstance().currentUser?.email)
+                        startActivity(intent)
+                    }
 
                     // else if successful
-                    Log.d("Register", "Successfully created user with uid: ${it.result!!.user.uid}")
+                    Log.d(TAG, "Successfully created user with uid: ${it.result?.user?.uid}")
+                    saveUserToFirebaseDatabase(it.toString())
                 }
                 .addOnFailureListener{
                     Log.d("Register", "Failed to create user: ${it.message}")
-                    Toast.makeText(this, "Failed to create user: \${it.message", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+    }
+
+    private fun saveUserToFirebaseDatabase(profileImageUrl: String) {
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+
+        val user = User(uid, username_edittext_register.text.toString())
+
+        ref.setValue(user)
+                .addOnSuccessListener {
+                    Log.d(TAG, "Finally we saved the user to Firebase Database")
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "Failed to set value to database: ${it.message}")
                 }
     }
 }
+
